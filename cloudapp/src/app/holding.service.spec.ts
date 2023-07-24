60f7632638242f330132242ed87a1bc7791c7e7d
little9 <jamie@jamielittle.org>
2023-07-24 09:25:23 -0500

    Don't use holding service

--- cloudapp/src/app/holding.service.spec.ts
+++ cloudapp/src/app/holding.service.spec.ts
@@ -1,16 +1,70 @@
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { CloudAppRestService, HttpMethod, Request } from '@exlibris/exl-cloudapp-angular-lib';
 import { HoldingService } from './holding.service';
+import { ItemUpdate } from './interfaces/item_update.interface';
+import { locationCodeMapping } from './main/location_code_mapping';
 
 describe('HoldingService', () => {
   let service: HoldingService;
+  let restService: CloudAppRestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spy = jasmine.createSpyObj('CloudAppRestService', ['call']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        HoldingService,
+        { provide: CloudAppRestService, useValue: spy }
+      ]
+    });
+
+    restService = TestBed.inject(CloudAppRestService);  
     service = TestBed.inject(HoldingService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should update holdings record', () => {
+    const itemUpdate: ItemUpdate = {
+      item: {
+        bib_data: {
+          title: 'Test Title',
+          mms_id: '123'
+        },
+        holding_data:
+          { holding_id: '456' }
+      },
+      library: 'elsc',
+      location: '2305open',
+      rmstBarcodeForItems: 'rmst01',
+      bigMoveMode: false,
+      libraryDesc: 'offsite storage',
+      locationDesc: 'offsite storage',
+      locationCodeMapping: locationCodeMapping
+    };
+
+    const holdingsRequest: Request = {
+      url: `/almaws/v1/bibs/${itemUpdate.item.bib_data.mms_id}/holdings/${itemUpdate.item.holding_data.holding_id}`,
+      method: HttpMethod.GET,
+      headers: {
+        Accept: "application/xml"
+      }
+    };
+
+    const holdingMock = '<root><datafield tag="852"><subfield code="b">LSC</subfield><subfield code="c">shmoffs</subfield></datafield></root>';
+
+    spyOn(restService, 'call').and.returnValue(of(itemUpdate.item));  // spyOn goes first
+
+    restService.call(holdingsRequest).subscribe(
+      holding => {
+        console.log(holding);
+      });
+  
+    service.updateHoldingsRecord(itemUpdate).subscribe(result => {
+      expect(result).toBeTruthy();
+    });
   });
 });
--- cloudapp/src/app/item.service.spec.ts
+++ cloudapp/src/app/item.service.spec.ts
@@ -1,16 +1,83 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CloudAppRestService } from '@exlibris/exl-cloudapp-angular-lib';
+import { of } from 'rxjs';
 
 import { ItemService } from './item.service';
+import { HoldingService } from './holding.service';
+import { Item } from './interfaces/item.interface';
+import { ItemUpdate } from './interfaces/item_update.interface';
+import { locationCodeMapping } from './main/location_code_mapping';
 
 describe('ItemService', () => {
   let service: ItemService;
+  let restServiceSpy: jasmine.SpyObj<CloudAppRestService>;
+  let holdingServiceSpy: jasmine.SpyObj<HoldingService>;
+
+  const mockItem: Item = {
+    bib_data: {
+      mms_id: 'mms_id',
+      title: 'title'
+    },
+    holding_data: {
+      holding_id: 'holding_id'
+    },
+    item_data: {
+      pid: 'pid',
+      storage_location_id: 'rmst01',
+    }
+  };
+
+  const mockItemUpdate: ItemUpdate = {
+    item: mockItem,
+    rmstBarcodeForItems: 'rmst02',
+    bigMoveMode: false,
+    location: 'location',
+    library: 'library',
+    libraryDesc: 'libraryDesc',
+    locationDesc: 'locationDesc',
+    locationCodeMapping: locationCodeMapping
+  };
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const restSpy = jasmine.createSpyObj('CloudAppRestService', ['call']);
+    const holdingSpy = jasmine.createSpyObj('HoldingService', ['updateHoldingsRecord']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        ItemService,
+        { provide: CloudAppRestService, useValue: restSpy },
+        { provide: HoldingService, useValue: holdingSpy }
+      ]
+    });
+
     service = TestBed.inject(ItemService);
+    restServiceSpy = TestBed.inject(CloudAppRestService) as jasmine.SpyObj<CloudAppRestService>;
+    holdingServiceSpy = TestBed.inject(HoldingService) as jasmine.SpyObj<HoldingService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getItemByBarcode should return item by barcode', () => {
+    const expectedItem = mockItem
+
+    restServiceSpy.call.and.returnValue(of(expectedItem));
+    service.getItemByBarcode('barcode').subscribe(item => {
+      expect(item).toEqual(mockItem);
+    });
+  });
+
+  it('#updateItem should return the updated item', () => {
+    const updatedItem = mockItem;
+    updatedItem.item_data.storage_location_id = 'rmst02'
+
+    restServiceSpy.call.and.returnValue(of(updatedItem));
+    service.updateItem(mockItemUpdate).subscribe(item => {
+      expect(item).toEqual(updatedItem);
+    });
   });
 });
